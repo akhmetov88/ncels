db48e315b526cb4cc24096c49495f13f573094c9
Arman <Arman@DESKTOP-JS4U3G1>
2017-08-18 15:26:25 +0600

    Добавлено поле "Класс" в справочник "Прейскурант цен"

--- SQLScripts/OBK/18082017_OBK_Ref_PriceList.sql
+++ SQLScripts/OBK/18082017_OBK_Ref_PriceList.sql
@@ -4,23 +4,27 @@ WHERE object_id = OBJECT_ID(N'[dbo].[OBK_Ref_PriceList]') AND type in (N'U'))
 BEGIN
 	CREATE TABLE [dbo].[OBK_Ref_PriceList](
 		[Id] uniqueidentifier NOT NULL,
-		[Type] INT NOT NULL,
+		[TypeId] INT NOT NULL,
 		[NameRu] NVARCHAR(255) NOT NULL,
 		[NameKz] NVARCHAR(255) NOT NULL,
-		[Unit] uniqueidentifier NOT NULL,
+		[UnitId] uniqueidentifier NOT NULL,
 		[Price] FLOAT NOT NULL,
-		[ServiceType] uniqueidentifier NOT NULL,
+		[ServiceTypeId] uniqueidentifier NOT NULL,
 		[IsDeleted] BIT NOT NULL,
+		[DegreeRiskId] uniqueidentifier NULL,
 		CONSTRAINT PK_OBK_Ref_PriceList
 		PRIMARY KEY ([Id]),
-		CONSTRAINT FK_OBK_Ref_PriceList__Type__OBK_Ref_Type__Id
-		FOREIGN KEY ([Type])
+		CONSTRAINT FK_OBK_Ref_PriceList__TypeId__OBK_Ref_Type__Id
+		FOREIGN KEY ([TypeId])
 		REFERENCES [OBK_Ref_Type]([Id]),
-		CONSTRAINT FK_OBK_Ref_PriceList__Unit__Dictionaries__Id
-		FOREIGN KEY ([Unit])
+		CONSTRAINT FK_OBK_Ref_PriceList__UnitId__Dictionaries__Id
+		FOREIGN KEY ([UnitId])
 		REFERENCES [Dictionaries]([Id]),
-		CONSTRAINT FK_OBK_Ref_PriceList__ServiceType__OBK_Ref_ServiceType__Id
-		FOREIGN KEY ([ServiceType])
-		REFERENCES [OBK_Ref_ServiceType]([Id])
+		CONSTRAINT FK_OBK_Ref_PriceList__ServiceTypeId__OBK_Ref_ServiceType__Id
+		FOREIGN KEY ([ServiceTypeId])
+		REFERENCES [OBK_Ref_ServiceType]([Id]),
+		CONSTRAINT FK_OBK_Ref_PriceList__DegreeRiskId__OBK_Ref_DegreeRisk__Id
+		FOREIGN KEY ([DegreeRiskId])
+		REFERENCES [OBK_Ref_DegreeRisk]([Id])
 	)
 END
