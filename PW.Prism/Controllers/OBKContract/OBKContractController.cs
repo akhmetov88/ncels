6fc6e16cb5eec59ada35190db50f74a4b19a86e1
Arman <Arman@DESKTOP-JS4U3G1>
2017-09-29 19:32:38 +0600

    OBK-170 Указание экспертной организации и подписывающего лица на внешнем портале

--- PW.Prism/Controllers/OBKContract/OBKContractController.cs
+++ PW.Prism/Controllers/OBKContract/OBKContractController.cs
@@ -52,7 +52,8 @@ namespace PW.Prism.Controllers.OBKContract
             ViewBag.UiId = Guid.NewGuid().ToString();
 
             var contractTypes = db.OBK_Ref_Type.Where(x => x.ViewOption == CodeConstManager.OBK_VIEW_OPTION_SHOW_ON_CREATE).OrderBy(x => x.Id).Select(o => new { o.Id, Name = o.NameRu, o.Code, o.NameKz });
-
+            var expertOrganizations = obkRepo.GetExpertOrganizations();
+            var signers = obkRepo.GetSigners();
 
             var countries = db.Dictionaries.Where(x => x.Type == "Country").ToList();
             var organizationForms = db.Dictionaries.Where(x => x.Type == "OpfType").ToList();
@@ -73,6 +74,8 @@ namespace PW.Prism.Controllers.OBKContract
             obkContract.ObkRsProductCount = productInfo.Count;
             ViewBag.Contract = obkContract;
             ViewBag.ContractTypes = new SelectList(contractTypes, "Id", "Name", obkContract.Type);
+            ViewBag.ExpertOrganizations = new SelectList(expertOrganizations, "Id", "Name", obkContract.ExpertOrganization);
+            ViewBag.Signers = new SelectList(signers, "Id", "Name", obkContract.Signer);
             ViewBag.Countries = new SelectList(countries, "Id", "Name", declarant.CountryId);
             ViewBag.OrganizationForms = new SelectList(organizationForms, "Id", "Name", declarant.OrganizationFormId);
             Guid selectedNonResident = Guid.Empty;
