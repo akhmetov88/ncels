b50253d9bdabc0be8f769ce0540c86750f46e000
Arman <Arman@DESKTOP-JS4U3G1>
2017-10-08 22:02:48 +0600

    Views/OBKContract/RefuseReasonDlg.cshtml Views/OBKContract/ShowRefuseReasonDlg.cshtml Views/OBKContract/UploadContract.cshtml

---
+++ PW.Ncels.Database/Entities/OBK_Ref_ContractHistoryStatus.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PW.Ncels.Database.DataModel
+{
+    public partial class OBK_Ref_ContractHistoryStatus
+    {
+        /// <summary>
+        /// Отправлен заявителем
+        /// </summary>
+        public const string SentByApplicant = "sentByApplicant";
+        /// <summary>
+        /// Распределен
+        /// </summary>
+        public const string SentToWork = "sentToWork";
+        /// <summary>
+        /// Соответствует требованиям
+        /// </summary>
+        public const string MeetsRequirements = "meetsRequirements";
+        /// <summary>
+        /// Не соответствует требованиям
+        /// </summary>
+        public const string DoesNotMeetRequirements = "doesNotMeetRequirements";
+        /// <summary>
+        /// Возвращен на доработку
+        /// </summary>
+        public const string Returned = "returned";
+        /// <summary>
+        /// Отправлен на согласование
+        /// </summary>
+        public const string SentToApproval = "sentToApproval";
+        /// <summary>
+        /// Отказано в согласовании
+        /// </summary>
+        public const string Refused = "refused";
+
+    }
+}
